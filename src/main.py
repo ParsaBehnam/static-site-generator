f70a57210dbd6f69d2649c9d1a782a1f180a38f2
ParsaBehnam <183300741+ParsaBehnam@users.noreply.github.com>
2026-04-19 23:43:12 +0300

    define main() and TextType Enum class & TextNode class in textnode.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,8 @@
-print('hello world')
+from textnode import TextType
+from textnode import TextNode
+
+def main():
+    text_node = TextNode("sample_text", TextType.BOLD, 'www.boot.dev')
+    print(text_node)
+
+main()
