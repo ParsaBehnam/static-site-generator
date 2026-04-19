7ca41936c22ab8185e30bdfad6ebcc12de16dafb
ParsaBehnam <183300741+ParsaBehnam@users.noreply.github.com>
2026-04-20 01:57:59 +0300

    define LeafNode & ParentNode classes

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -20,5 +20,37 @@ class HtmlNode:
     
     def __repr__(self):
         return f"HtmlNode({self.tag}, {self.value}, {self.children}, {self.props})"
+
+class LeafNode(HtmlNode):
+    def __init__(self, tag, value, props = None):
+        super().__init__(tag, value, None, props)
+        
+    def to_html(self):
+        if not self.value:
+            raise ValueError('text is missing')
+        
+        if not self.tag:
+            return self.value
+        
+        return f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>'
     
+    def __repr__(self):
+        return f'LeafNode({self.tag}, {self.value}, {self.props})'
     
+class ParentNode(HtmlNode):
+    def __init__(self, tag, children, props = None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if not self.tag:
+            raise ValueError('tag is missing')
+        
+        if not self.children:
+            raise ValueError('no child tag found')
+        
+        string = ''
+
+        for child in self.children:
+            string += child.to_html()
+
+        return f'<{self.tag}{self.props_to_html()}>{string}</{self.tag}>'
