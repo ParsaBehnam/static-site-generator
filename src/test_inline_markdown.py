1fc34be2d12b5c4f0462a45df4266ba7fd8c7a30
ParsaBehnam <183300741+ParsaBehnam@users.noreply.github.com>
2026-04-26 07:29:32 +0300

    define text_to_textnode function in inline_markdown.py

---
+++ src/test_inline_markdown.py
@@ -0,0 +1,11 @@
+import unittest
+from inline_markdown import text_to_textnode
+from textnode import TextNode, TextType
+
+class TestInlineMarkdown(unittest.TestCase):
+     def test_text_to_textnode(self):
+        node = [TextNode("this is ", TextType.TEXT), TextNode('bold', TextType.BOLD), TextNode(' and ', TextType.TEXT),
+                TextNode('italic', TextType.ITALIC)]
+        node2 = text_to_textnode('this is **bold** and _italic_')
+
+        self.assertEqual(node, node2)
